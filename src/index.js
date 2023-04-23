43896ffa4c9a8d7a8772b81e05f9fd82b99880d1
KiruiTito <titokirui402@gmail.com>
2023-04-23 10:35:33 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React, { useState } from 'react';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function App() {
+  const [foods, setFoods] = useState(['Pizza', 'Burger', 'Fries']);
+
+  const handleLiClick = (index) => {
+    const newFoods = [...foods];
+    newFoods.splice(index, 1);
+    setFoods(newFoods);
+  };
+
+  return (
+    <div>
+      <h1>My favorite foods</h1>
+      <ul>
+        {foods.map((food, index) => (
+          <li key={index} onClick={() => handleLiClick(index)}>
+            {food}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
